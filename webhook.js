c2febf8b6cd344896ad5f25fb4cad2a7334fb67c
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-06-20 20:13:02 +0200

    edit

--- webhook.js
+++ webhook.js
@@ -6,8 +6,8 @@ var exec  = require('child_process').exec;
 var port  = 8433;
 
 var options = {
-  key: fs.readFileSync('encription/privateKey.key'),
-  cert: fs.readFileSync('encryption/certificate.crt')
+  key: fs.readFileSync('encription/private.key'),
+  cert: fs.readFileSync('encryption/mydomain.crt')
 };
 
 https.createServer(options, function (req, res) {
