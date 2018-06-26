189491f7085c1d3d96cb66b082d59a0eba7dfb4b
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-06-26 10:12:11 +0200

    edit

--- webhook.js
+++ webhook.js
@@ -22,11 +22,10 @@ https.createServer(options, function (request, response) {
     	let body = [];
     	request.on('data', (chunk) => {
     		body.push(chunk);
-    	}).on('end', () => {
-    		body = Buffer.concat(body).toString();
-    		d = JSON.parse(body);
-        
-        path = '/home/pi' + d.repository.name;
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            d = JSON.parse(body);
+            path = '/home/pi/' + d.repository.name;
 
     		//console.log(d.clone_url);
     		//console.log(d);
